9870227e17dfd2f7d3b7960f5715884aadc92d7a
Sumit-Kumar04 <sumitkumar4102004b@gmail.com>
2026-02-20 15:34:00 +0530

    Revision

--- Node/day1/app.js
+++ Node/day1/app.js
@@ -1,3 +1,5 @@
+//V-8 ENGINE   
+
 //Non-blocking
 console.log("Hi i am js");        //java script is single threaded
 console.log("Hi i am js");
@@ -17,5 +19,31 @@ console.log(1+ +1);
 console.log(1+ -'1');    //Unary negation
 
 
-//v8-->parsing(checks syntax of code and code convert to machine language ) compiling(during compiling JIT )  execution   libubi(multithreading access to os file system )                   
+//v8-->parsing(checks syntax of code and code convert to machine language ) compiling(during compiling JIT(jUST IN TIME COMPILATION) )  execution   libubi(multithreading access to os file system )                   
 //Request based system
+
+
+//js single threades
+//node--> multithreaded
+//Request driven-->processed by server 
+//Event driven system-->completion 
+//call stack
+//call back queue event loop
+//micro macro task
+//call back function
+//call back  hell
+//promise  ->success failure pending 
+//try catch 
+//then 
+//chaining
+//Package 
+
+//node  package json
+//dependency dev_dependency
+//dependencies version-->major.minor.patch           ^ ~
+//utils same file name cotact
+
+//module.exports=func          default common js
+//module.exports={name,name2}         name common js
+//eport varName  //exac anem in inmport
+//eport  default varName  //can change name
--- Node/day8/app.js
+++ Node/day8/app.js
@@ -1,9 +1,11 @@
 const http = require("http");
+const os=require('os');
 const server = http.createServer((req, res) => {
     const requestUrl = req.url;
     console.log("request ru ", requestUrl);
     const requestMethod = req.method;
     console.log("res method ", requestMethod);
+    //Check if log.txt is available 
     if (requestUrl === "/home") {
         res.end("hello World------homr -----");
     }
@@ -12,9 +14,12 @@ const server = http.createServer((req, res) => {
     if (requestUrl === "/user") {
         res.end("User PAge")
     }
+
+    if(requestUrl==='/systemconfig'){
+        const freememory=`Free ram :&{os.freemem() /1024 /1024 1024}`
+        res.end(freememory);
+    }
     res.end("hello World-----------");
-
-
 
 });
 
