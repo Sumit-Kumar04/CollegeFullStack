cd715957400120c42a2b2144e38f669473f0d476
Sumit-Kumar04 <sumitkumar4102004b@gmail.com>
2026-03-30 12:10:58 +0530

    Donw with ejs data transfering in single file

--- Node/day10/index.js
+++ Node/day10/index.js
@@ -5,7 +5,39 @@ app.set("view engine","ejs");
 
 
 app.get("/",(req,res)=>{
-        res.render("main");
+    const name="Sumit";
+
+    const arr = [
+        {
+            name: "Ram",
+            section: "E"
+        },
+        {
+            name: "Shyam",
+            section: "E"
+        }
+    ];
+        res.render("main",{name,obj});
+});
+
+const webName="Unstop";
+const work="internship";
+ const msg="Made with love";
+app.get("/header",(req,res)=>{
+    
+    res.render("header",{webName});
+})
+app.get("/body",(req,res)=>{
+    
+    res.render("body",{work});
+})
+app.get("/footer",(req,res)=>{
+   
+    res.render("footer",{msg});
+})
+app.get("/home",(req,res)=>{
+   
+    res.render("homePage",{webName,work,msg});
 })
 
 
